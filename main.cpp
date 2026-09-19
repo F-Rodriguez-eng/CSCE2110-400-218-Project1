29921286f772e32bc1a47c901111de2d389cf48d
F-Rodriguez-eng <fernandorodriguez7@my.unt.edu>
2026-09-19 15:37:04 -0500

    Add initial class definitions for WaitingList, CancelationHistory, and ReportGenerator

---
+++ main.cpp
@@ -0,0 +1,23 @@
+//WAITING LIST
+class WaitingList{
+  private:
+
+  public:
+
+};
+
+//CANCELATION
+class CancelationHistory{
+  private:
+
+  public:
+
+};
+
+//REPORT GEN
+class ReportGenerator{
+  private:
+
+  public:
+
+};
