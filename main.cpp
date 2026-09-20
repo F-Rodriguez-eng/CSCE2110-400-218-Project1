e2b10f2c90be2c6b8228df5b61d0b2701100de00
cheruiyotbrenda3-ship-it <cheruiyotbrenda3@gmail.com>
2026-09-20 16:41:45 -0500

    Refactor main.cpp to include headers and update WaitingList
    Added includes and defined member variables for WaitingList class.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
+#include Resource.h
+#include Resource.cpp
+#include Reservation.cpp
+#include Reservation.h
+#include ReservationManagement.cpp
+#include ReservationMnagement.h
+#include <iostream>
+#include <string>  
+
+using namespace std;
+
 //WAITING LIST
 class WaitingList{
   private:
-
+string studentID
+string studentName
+string resource ID
+string requestDate;
   public:
 
 };
