77d59a458063427cabf316345591b79ad781097a
F-Rodriguez-eng <FernandoRodriguez7@my.unt.edu>
2026-09-20 14:53:26 -0500

    Continued on ReservationManagement

--- ReservationManagement.cpp
+++ ReservationManagement.cpp
@@ -17,3 +17,78 @@ ReservationManager::~ReservationManager(){
 }
 
 //creates Reservation
+void ReservationManager::createReserv(Reservation r){
+  //create new node containing reservation
+  ReservationNode newNode = new ReservationNode(r);
+
+  //insert node infront of list
+  newNode->next = head;
+  head = newNode;
+
+  cout << "Your reservation was created successfully." << endl;
+
+}
+
+//displays all active reservations
+void ReservationManager::viewReservations(){
+  //check if list is empty
+  if (head == nullptr){
+    cout << "No reservations in line." << endl;
+    return;
+  }
+
+  ReservationNode* current = head;
+
+  //traverse linked list
+  while (current != nullptr){
+
+    //display reservation info
+    current -> data.display();
+
+    cout << "********************" << endl;
+
+    current = current->next;
+  }
+
+}
+
+Reservation* ReservationManager::searchReserv(int reservationID){
+  ReservationNode* current = head;
+
+  //traverse linked list
+  while (current != nullptr){
+    //checking if IDs match
+    if (current->data.getReservationID() == reservationID){
+
+      return &(current->data);
+    }
+
+    current = current->next;
+  }
+
+  //no matching reservation was found
+  return nullptr;
+}
+
+//cancels reservation
+bool ReservationManager::cancelReserv(int reservationID){
+  ReservationNode* current = head;
+  ReservationNode* previous = nullptr;
+
+  //search via traversal
+  while (current != nullptr){
+    
+    if (current->data.getReservationID() == reservationID){
+
+      //save reservation to stack
+      cancelHistory.push(current->data);
+
+      //remove first node
+      
+
+      //remove mid or last node
+    }
+
+  }
+
+}
